2ccfa2ba268babc8dfba70294dd8c5eb7e6ddefb
aerdon085 <adrianluzon36@gmail.com>
2021-09-25 10:10:21 +0800

    Getting user input
    Need more practice! Find materials online!

--- Test/test.c
+++ Test/test.c
@@ -4,21 +4,29 @@
 
 int main()
 {
-    char name[] = "Adrian"; // %s
-    char grade = 'N';       // %c
-    int age = 19;           // %d
-    double gpa = 3.7;       // %f
+    // variables (char"", char'', int, double/float)
+    char name[] = "Adrian";      // %s
+    char faveLetter = 'S';       // %c
+    int age = 19;                // %d
+    double /*or float*/ g = 9.8; // %f
+
+    // constant variables (const)
+    const /*const mean a variable (in this case an integer) whose value cannot be reassigned*/ int BIRTHYEAR = 2002;
+    char birthMonth[] = "March"; // char x[]="strings" are an exception, they inherently cannot be reassigned
+    const int BIRTHDAY = 25;     // it is ideal that all constant variables are denoted by capitalized names to avoid confusion
 
     // \n makes string before it go to new line in output
-    printf("%d\n", age);
-    printf("%f\n", gpa);
-    printf("%s\n", name);
-    printf("My name is %s and I am %d\n", "Adrian", 19);
+    faveLetter = 'A';
 
-    printf("%f\n", pow(2, 3)); // always %f even if result is non-decimal; dunno why
-    printf("%f\n", sqrt(36));
-    printf("%f\n", ceil(gpa));
-    printf("%f\n", floor(gpa));
+    printf("My name is %s %s and I am %d years old.\n", name, "Luzon", age);
+    printf("My favourite letter is %c.\n", faveLetter);
+    printf("My birthdate is %s %d %d.\n", birthMonth, BIRTHDAY, BIRTHYEAR);
+
+    printf("\nGravity is %f.\n", g);
+    printf("My age plus year of birth is %d.\nIf result above is divided by day of birth: %f.\n", age + BIRTHYEAR, 2021 / BIRTHDAY);
+    printf("%d to the power of %d is %f.\n", 2, 3, pow(2, 3)); // always %f and not %d placeholder for math functions even if result is non-decimal, dunno why
+    printf("The square root of my day of birth is %f using percentF as placeholder.\nUsing percentD as placeholder: %d.\n", sqrt(BIRTHDAY), sqrt(BIRTHDAY));
+    printf("Rounding up the value of g, the answer is %f. Rounding it down: %f.\n", ceil(g), floor(g));
 
     return 0;
 }
