d71b5e9fb36810925844c92185080d72a429dd03
aerdon085 <adrianluzon36@gmail.com>
2021-10-02 16:35:52 +0800

    building a better calculator

--- Test/exercise.c
+++ Test/exercise.c
@@ -7,14 +7,18 @@ int sayHi();
 
 int main()
 {
-    printf("%d", sayHi(3));
+    printf("%d", sayHi(3,4));
 
     return 0;
 }
 
-int sayHi(int num)
+int sayHi(int num1, int num2)
 {
-    int result = num * num * num;
+    int result;
+    if(num1 > num2){
+        result = num1;
+    } else {
+        result = num2;}
 
     return result;
 }
