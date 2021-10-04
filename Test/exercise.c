6b376ebfb7af824ace221319a5889b440b07bfe4
aerdon085 <adrianluzon36@gmail.com>
2021-10-04 09:46:17 +0800

    10/4/2021
    new source materials:
    - youutube C
    - book C beginner
    - exercise w3schools

--- Test/exercise.c
+++ Test/exercise.c
@@ -2,23 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* Function signature */
-int sayHi();
-
 int main()
 {
-    printf("%d", sayHi(3,4));
+    printf("Nice\a!\n");
 
     return 0;
-}
-
-int sayHi(int num1, int num2)
-{
-    int result;
-    if(num1 > num2){
-        result = num1;
-    } else {
-        result = num2;}
-
-    return result;
 }
