f1f45906b2d306381ce2519fe49f6a5a0d4046b3
aerdon085 <adrianluzon36@gmail.com>
2021-10-05 08:51:27 +0800

    Adding Words to Your Programs
    new source materials:
    - youutube C
    - book C beginner
    - exercise w3schools

--- Test/exercise.c
+++ Test/exercise.c
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
+// signature
+double calc();
+
 int main()
 {
-    printf("Nice\a!\n");
+    char firstName;
+    char middleName;
+    int input1;
+    int input2;
+
+    printf("First initial name of customer: ");
+    scanf(" %c", &firstName);
+    printf("Middle initial of customer: ");
+    scanf(" %c", &middleName);
+    printf("Desired number of pencils: ");
+    scanf("%d", &input1);
+    printf("Desired number of notebooks: ");
+    scanf("%d", &input2);
+
+    printf("%c.%c. needs %d pencils, %d notebooks, and 1 lunchbox. Total amount is %.2f.", firstName, middleName, input1, input2, calc(input1, input2));
 
     return 0;
+}
+
+double calc(int noPencils, int noNotes)
+{
+    const double pricePencils = 0.23;
+    const double priceNotes = 2.89;
+    const double priceLunch = 4.99;
+
+    return (noPencils * pricePencils) + (noNotes * priceNotes) + priceLunch;
 }
